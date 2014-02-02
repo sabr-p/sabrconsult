55630783bbd049746430f8ea4170eaa2ba1b5dad
deimosaffair <deimosaffair@debiandmz.dmz>
2014-02-02 17:49:30 +0000

    refactor

--- footer.php
+++ footer.php
@@ -2,25 +2,24 @@
 	<div id="f">
 		<div class="container">
 			<div class="row">
-				<!-- ADDRESS -->
 				<div class="col-lg-3">
 					<h4>we are at</h4>
 					<p>
-						contact@sabrcosnult.com<br/>
+						contact@sabrconsult.com<br/>
 					</p>
 				</div><! --/col-lg-3 -->
 
 				<div class="col-lg-3">
 					<h4>Contact Us</h4>
 					<p>
-            <a href="mailto:contact.us@sabrconsult.com">contact.us@sabrconsult.com</a><br/>
+            <a href="mailto:contact@sabrconsult.com">contact@sabrconsult.com</a><br/>
 					</p>
 				</div><! --/col-lg-3 -->
 
 				<div class="col-lg-3">
 					<h4>Info</h4>
 					<p>
-						<i class="fa fa-angle-right"></i> <a href="https://github.com/sabr-p">Code for Invictus.io</a><br/>
+						<i class="fa fa-angle-right"></i> <a href="https://github.com/sabr-p/sabrconsult">Code for sabrconsult</a><br/>
 					</p>
 				</div><!-- /col-lg-3 -->
 
@@ -32,23 +31,5 @@
 
 
     <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/retina.js"></script>
-    <script src="assets/js/invictus.js"></script>
-	<script>
-		$(window).scroll(function() {
-			$('.si').each(function(){
-			var imagePos = $(this).offset().top;
 
-			var topOfWindow = $(window).scrollTop();
-				if (imagePos < topOfWindow+400) {
-					$(this).addClass("slideUp");
-				}
-			});
-		});
-	</script>
-    <script>
-	    $('#myTab a').click(function (e) {
-		  e.preventDefault()
-		  $(this).tab('show')
-		})
 	</script>
